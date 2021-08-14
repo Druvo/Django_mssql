eb31165caaeca11dbcbf8a2b72b5d04701f943bc
Zahid Hasan <zhdruvo@gmail.com>
2021-08-15 04:23:53 +0600

    Initial commit

---
+++ Django_mssql/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\zhdru\source\repos\Django_mssql\Django_mssql\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
